c5cfb77e13ebe7956e354b08ae2280d65822412e
deaiko <s.deaiko@gmail.com>
2023-02-05 22:23:08 +0600

    ed. text 2.0.

--- JavaScript для начинающих 2022/js/25-event.js
+++
@@ -1,8 +0,0 @@
-// ================================  Объект event   ===============================
-
-const list = document.querySelector('#list');
-list.addEventListener('click', function (event) {
-	console.log(this);
-	console.log(event);
-	console.log(event.target);
-});
